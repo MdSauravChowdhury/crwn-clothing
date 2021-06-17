f0c582f66c27cf483e762caf9d62223ef49a22a4
MdSauravChowdhury <mdsauravchowdhury@gmail.com>
2021-06-17 10:44:10 +0600

    update our meny item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import HomePage from './components/pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom'
+
+const HatPage = (props) => {
+  return (
+    <div>
+        <button onClick={() => props.history.push('/')}>Hats</button>
+
+      <h1>this is hat pages</h1>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route  path='/hats' component={HatPage}/>          
+        </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,33 +12,37 @@ class Directory extends Component {
                     title: "hats",
                     imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
                     id: 1,
-                    linkUrl: "shop/hats",
+                    linkUrl: "hats",
                 },
                 {
                     title: "jackets",
                     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
                     id: 2,
-                    linkUrl: "shop/jackets",
+                    linkUrl: "",
+                    // linkUrl: "shop/jackets",
                 },
                 {
                     title: "sneakers",
                     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
                     id: 3,
-                    linkUrl: "shop/sneakers",
+                    linkUrl: "",
+                    // linkUrl: "shop/sneakers",
                 },
                 {
                     title: "womens",
                     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
                     size: "large",
                     id: 4,
-                    linkUrl: "shop/womens",
+                    linkUrl: "",
+                    // linkUrl: "shop/womens",
                 },
                 {
                     title: "mens",
                     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
                     size: "large",
                     id: 5,
-                    linkUrl: "shop/mens",
+                    linkUrl: "",
+                    // linkUrl: "shop/mens",
                 },
             ],
         };
@@ -47,12 +51,10 @@ class Directory extends Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map((section) => (
+                {this.state.sections.map(({ id, ...otherSectionProps }) => (
                     <MenuItem
-                        key={section.id}
-                        title={section.title}
-                        imageUrl={section.imageUrl}
-                        size={section.size}
+                        key={id}
+                        { ...otherSectionProps }
                     />
                 ))}
             </div>
